93476cb144c350fb84e164457ba293e334a74807
UnclassedPenguin <tvanden@live.com>
2022-01-24 14:17:22 -0700

    fixed order, cleaned up some things. Missing semi-colons

--- hearts.js
+++ hearts.js
@@ -13,7 +13,7 @@
 
 // Global Variables
 // List of players
-const players = [];
+var players = [];
 // Max score to play the game to, this is set later on by the player
 var maxScore = 0;
 // Trick number, helps keep track of which direction to pass cards
@@ -34,6 +34,7 @@ var monthname = ["January", "February", "March", "April", "May", "June",
                  "July", "August", "September", "October", "November", "December"];
 var monthint = d.getMonth();
 var month = monthname[monthint];
+var year = d.getFullYear()
 
 // Function to display time correctly ie 09, instead of 9
 function addZero(i) {
@@ -45,20 +46,20 @@ let h = addZero(d.getHours());
 let m = addZero(d.getMinutes());
 let time = h + ":" + m;
 
-var datestring = dow + ", " + month + " " + day + " " +  time;
+var datestring = dow + ", " + month + " " + day + " " + year + " " + time;
 document.getElementById("date").innerHTML = datestring;
 
 
 // Function to clear out the main-content div
 function clearDiv() {
-  document.getElementById("main-content").innerHTML = ""
+  document.getElementById("main-content").innerHTML = "";
 }
 
 
 //Start of "program", asks for names of players
 function showNameFields() {
   document.getElementById("main-content").innerHTML +=
-    "<h5>Please Enter 3-5 players</h5><form><p class='rowp'><label for='player1'>Player 1: </label><input type='text' id='player1' name='players'></p><p class='rowp'><label for='player2'>Player 2: </label><input type='text' id='player2' name='players'></p><p class='rowp'><label for='player3'>Player 3: </label><input type='text' id='player3' name='players'></p><p class='rowp'><label for='player4'>Player 4: </label><input type='text' id='player4' name='players'></p><p class='rowp'><label for='player5'>Player 5: </label><input type='text' id='player5' name='players'></p></form><button type='button' class='btn btn-light btn-small' id='getNamesButton'>Set Players</button>"
+    "<h5>Please Enter 3-5 players</h5><form><p class='rowp'><label for='player1'>Player 1: </label><input type='text' id='player1' name='players'></p><p class='rowp'><label for='player2'>Player 2: </label><input type='text' id='player2' name='players'></p><p class='rowp'><label for='player3'>Player 3: </label><input type='text' id='player3' name='players'></p><p class='rowp'><label for='player4'>Player 4: </label><input type='text' id='player4' name='players'></p><p class='rowp'><label for='player5'>Player 5: </label><input type='text' id='player5' name='players'></p></form><button type='button' class='btn btn-light btn-small' id='getNamesButton'>Set Players</button>";
   document.getElementById("getNamesButton").addEventListener("click", getNames, false);
 }
 
@@ -99,7 +100,7 @@ function getNames() {
   players.forEach(checkEmpty);
   players.forEach(checkEmpty);
   players.forEach(createPlayer);
-  clearDiv()
+  clearDiv();
   showScoreField();
 }
 
@@ -107,7 +108,7 @@ function getNames() {
 // Displays html to ask for input on what the Max Score should be
 function showScoreField() {
   document.getElementById("main-content").innerHTML =
-    "<br><h5>Score to play to (ie 100)</h5>\<br><label for='score'>Score: </label>\<input type='number' value=100 id='score' name='score'>\<br><button type='button' class='btn btn-light btn-small' id='getScoreValue'>Set Score</button>" 
+    "<br><h5>Score to play to (ie 100)</h5>\<br><label for='score'>Score: </label>\<input type='number' value=100 id='score' name='score'>\<br><button type='button' class='btn btn-light btn-small' id='getScoreValue'>Set Score</button>";
   document.getElementById("getScoreValue").addEventListener("click", getScoreValue, false);
 }
 
@@ -119,6 +120,85 @@ function getScoreValue() {
   //console.log("Score: " + maxScore);
   clearDiv();
   showMainScreen();
+}
+
+
+// This is the main loop where scores are taken and added up
+// until someone breaks maxScore
+function showMainScreen() {
+  
+  // Displays players score updates on button click
+  function showPlayersScore(value, index, array) {
+    htmlstring = "<tr><th class='righta'>" + window["player" + index].name + ": </th><th>" + window["player" + index].score + "</th></tr>";
+    html += htmlstring;
+  }
+
+  // This displays the input fields to gather the scores 
+  function showScore(value, index, array) {
+    htmlstring = "<tr><th class='righta'>" + window["player" + index].name + ": </th><th>" + "<input type='number' value=0 id='" + window["player" + index].name + "score' name='score'></th></tr>";
+    html += htmlstring;
+  }
+
+  // This gets the values of the input fields and adds to each players total score 
+  // player.score
+  function addScore(value, index, array) {
+    //console.log("Add Score for player " + window["player" + index].name)
+    trickpoints = document.getElementById(window["player" + index].name + 'score').value;
+    //console.log("Trickpoints: " + trickpoints)
+    //console.log("Trickpoints type: " + typeof(trickpoints))
+    let trickpointint = parseInt(trickpoints);
+    //console.log("Trickpointint: " + trickpointint)
+    //console.log("Trickpointint type: " + typeof(trickpointint))
+    window["player" + index].score += trickpointint;
+  }
+
+  // This runs every "loop" and checks player.score values against
+  // maxScore to see if anyone has lost, if they have, 
+  // it sets gameOver to true
+  function checkScore(value, index, array) {
+    if (window["player" + index].score >= maxScore) {
+      //console.log("PLAYER HAS LOST DO SOMETHING ABOUT IT");
+      window["player" + index].lose = true;
+      gameOver = true;
+    } else {
+      //console.log("GAME CONTINUES");
+    }
+  }
+
+  // Function that runs when button is pressed. Calls other functions 
+  // to add scores and check scores against maxScore if gameOver is
+  // equal to true here it switches to gameIsOver function to display
+  // final scores and who has won the game
+  function scoreButton() {
+    players.forEach(addScore);
+    players.forEach(checkScore);
+    trick += 1;
+    if (gameOver == false) {
+      showMainScreen();
+    } else {
+      gameIsOver();
+    }
+
+  }
+
+  // This is what shows all of the info for the game
+  // Above this are all functions to do all of the things :P
+  let html = "<h5>Game: </h5>";
+  if (trick % 2 == 0) {
+    html += "<p>Pass Cards Right</p>";
+  } else {
+    html += "<p>Pass Cards Left</p>";
+  }
+  html += "<table><tr><th>Trick: </th><th>" + trick + "</th></tr>";
+  html += "<tr><th>Points: </th><th></th></tr>";
+  players.forEach(showPlayersScore); 
+  html += "<tr><th> <h5> Add Points: </h5> </th></tr>";
+  players.forEach(showScore); 
+  html += "</table>";
+  addButton = "<br><div class='text-center'><button type='button' class='btn btn-light btn-small' id='addScore'>Add To Score</button></div>";
+  html += addButton;
+  document.getElementById("main-content").innerHTML = html;
+  document.getElementById("addScore").addEventListener("click", scoreButton, false);
 }
 
 
@@ -178,10 +258,10 @@ function gameIsOver() {
   clearDiv();
   let html = "<h4>Game Over!</h4><br>";
   html += "<h5>Final Points:</h5>";
-  html += "<table>"
+  html += "<table>";
   players.forEach(showPlayersScore);
-  html += "</table>"
-  let playersFinalScore = []
+  html += "</table>";
+  let playersFinalScore = [];
   players.forEach(getFinalScore);
   //console.log("PLAYERS FINAL SCORE: " + playersFinalScore);
   let min = findMin();
@@ -191,85 +271,6 @@ function gameIsOver() {
   html += addButton;
   document.getElementById("main-content").innerHTML = html;
   document.getElementById("playAgain").addEventListener("click", startAgain, false);
-}
-
-
-// This is the main loop where scores are taken and added up
-// until someone breaks maxScore
-function showMainScreen() {
-  
-  // Displays players score updates on button click
-  function showPlayersScore(value, index, array) {
-    htmlstring = "<tr><th class='righta'>" + window["player" + index].name + ": </th><th>" + window["player" + index].score + "</th></tr>";
-    html += htmlstring;
-  }
-
-  // This displays the input fields to gather the scores 
-  function showScore(value, index, array) {
-    htmlstring = "<tr><th class='righta'>" + window["player" + index].name + ": </th><th>" + "<input type='number' value=0 id='" + window["player" + index].name + "score' name='score'></th></tr>";
-    html += htmlstring;
-  }
-
-  // This gets the values of the input fields and adds to each players total score 
-  // player.score
-  function addScore(value, index, array) {
-    //console.log("Add Score for player " + window["player" + index].name)
-    trickpoints = document.getElementById(window["player" + index].name + 'score').value;
-    //console.log("Trickpoints: " + trickpoints)
-    //console.log("Trickpoints type: " + typeof(trickpoints))
-    let trickpointint = parseInt(trickpoints)
-    //console.log("Trickpointint: " + trickpointint)
-    //console.log("Trickpointint type: " + typeof(trickpointint))
-    window["player" + index].score += trickpointint;
-  }
-
-  // This runs every "loop" and checks player.score values against
-  // maxScore to see if anyone has lost, if they have, 
-  // it sets gameOver to true
-  function checkScore(value, index, array) {
-    if (window["player" + index].score >= maxScore) {
-      //console.log("PLAYER HAS LOST DO SOMETHING ABOUT IT");
-      window["player" + index].lose = true;
-      gameOver = true;
-    } else {
-      //console.log("GAME CONTINUES");
-    }
-  }
-
-  // Function that runs when button is pressed. Calls other functions 
-  // to add scores and check scores against maxScore if gameOver is
-  // equal to true here it switches to gameIsOver function to display
-  // final scores and who has won the game
-  function scoreButton() {
-    players.forEach(addScore);
-    players.forEach(checkScore);
-    trick += 1;
-    if (gameOver == false) {
-      showMainScreen();
-    } else {
-      gameIsOver();
-    }
-
-  }
-
-  // This is what shows all of the info for the game
-  // Above this are all functions to do all of the things :P
-  let html = "<h5>Game: </h5>";
-  if (trick % 2 == 0) {
-    html += "<p>Pass Cards Right</p>";
-  } else {
-    html += "<p>Pass Cards Left</p>";
-  }
-  html += "<table><tr><th>Trick: </th><th>" + trick + "</th></tr>";
-  html += "<tr><th>Points: </th><th></th></tr>"
-  players.forEach(showPlayersScore); 
-  html += "<tr><th> <h5> Add Points: </h5> </th></tr>";
-  players.forEach(showScore); 
-  html += "</table>"
-  addButton = "<br><div class='text-center'><button type='button' class='btn btn-light btn-small' id='addScore'>Add To Score</button></div>";
-  html += addButton;
-  document.getElementById("main-content").innerHTML = html;
-  document.getElementById("addScore").addEventListener("click", scoreButton, false);
 }
 
 
